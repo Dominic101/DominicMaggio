949a5c3df34074ce7e13370a7b0b33288a58d505
Dominic Maggio <drmaggio@mit.edu>
2025-02-11 13:21:58 -0500

    working on setting up branch

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,7 @@ const sass = require("sass");
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPlugin(eleventySass, { sass });
     return {
-        pathPrefix: "/personal-website-demo",
+        pathPrefix: "/DominicMaggio",
         dir: {
             includes: "_templates",
             output: "_site",
